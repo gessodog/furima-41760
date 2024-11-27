9fe083175ae5b743771a1f31338a3969516a6762
gessodog <kai.okumura56@gmail.com>
2024-11-27 20:08:58 +0900

    バリデーションとエラーメッセージの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,10 @@ class User < ApplicationRecord
   validates :last_name_kana,  presence: true
   validates :birth,           presence: true
   validate :password_complexity
+  validate :first_name_zenkaku
+  validate :last_name_zenkaku
+  validate :first_name_kana_zenkaku
+  validate :last_name_kana_zenkaku
 
   
   private
@@ -18,6 +22,30 @@ class User < ApplicationRecord
   def password_complexity
     return if password.blank? || password =~ /^(?=.*[a-zA-Z])(?=.*[0-9]).{6,}$/
 
-    errors.add :password, 'must contain at least 6 characters, including one letter, one number'
+    errors.add :password, 'must including one letter, one number'
+  end
+
+  def first_name_zenkaku
+    return if first_name.blank? || first_name =~ /\A[ぁ-んァ-ヶ一-龠々ー]+\z/ 
+
+    errors.add :first_name, 'must be full-width characters'
+  end
+
+  def last_name_zenkaku
+    return if last_name.blank? || last_name =~ /\A[ぁ-んァ-ヶ一-龠々ー]+\z/ 
+
+    errors.add :last_name, 'must be full-width characters'
+  end
+
+  def first_name_kana_zenkaku
+    return if first_name_kana.blank? || first_name_kana =~ /\A[ァ-ヶー]+\z/ 
+
+    errors.add :first_name_kana, 'must be full-width characters'
+  end
+
+  def last_name_kana_zenkaku
+    return if last_name_kana.blank? || last_name_kana =~ /\A[ァ-ヶー]+\z/ 
+
+    errors.add :last_name_kana, 'must be full-width characters'
   end
 end
