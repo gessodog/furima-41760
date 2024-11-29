8d1c1dbe64735db21b4723fd4e0521c75a985f26
gessodog <kai.okumura56@gmail.com>
2024-11-29 17:28:58 +0900

    rubocopの適用

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,24 +18,31 @@ class User < ApplicationRecord
 
   def first_name_complexity
     return if first_name.blank? || first_name =~ /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
+
     errors.add :first_name, 'must be full-width characters(Kanji,Hiragana,Katakana)'
   end
+
   def last_name_complexity
     return if last_name.blank? || last_name =~ /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
+
     errors.add :last_name, 'must be full-width characters(Kanji,Hiragana,Katakana)'
   end
 
   def first_name_kana_complexity
     return if first_name_kana.blank? || first_name_kana =~ /\A[ァ-ヶー]+\z/
+
     errors.add :first_name_kana, 'must be full-width characters(Katakana)'
   end
+
   def last_name_kana_complexity
     return if last_name_kana.blank? || last_name_kana =~ /\A[ァ-ヶー]+\z/
+
     errors.add :last_name_kana, 'must be full-width characters(Katakana)'
   end
 
   def password_complexity
-    return if password.blank? || password =~ /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+    return if password.blank? || password =~ /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+
     errors.add :password, 'must including one letter, one number'
   end
 end
