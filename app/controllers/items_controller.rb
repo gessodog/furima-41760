5cc88748415760dbf14262c60efcfb38591bc043
gessodog <kai.okumura56@gmail.com>
2024-11-30 18:06:14 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :item_information, :item_category_id, :item_state_id, :delivery_cost_id, :prefecture_id, :delivery_day_id, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,17 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_category
+
+  validates :item_name,        presence: true
+  validates :item_information, presence: true
+  validates :item_category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :item_state_id,    numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_cost_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id,    numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_day_id,  numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price,            presence: true
+  validates :image,            presence: true
 end
+
