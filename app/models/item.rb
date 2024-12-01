5885ad87d3fc8309d39db93dd69ae2159f3d8747
gessodog <kai.okumura56@gmail.com>
2024-12-01 18:45:30 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,13 @@
 class Item < ApplicationRecord
-  belongs_to :user
-  has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :item_category
+  belongs_to :item_state
+  belongs_to :delivery_cost
+  belongs_to :prefecture
+  belongs_to :delivery_day
+
+  belongs_to :user
+  has_one_attached :image
 
   validates :item_name,        presence: true
   validates :item_information, presence: true
