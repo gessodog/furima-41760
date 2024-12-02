9b7037aba947215672ec9cf205dc66046d0ab3d0
gessodog <kai.okumura56@gmail.com>
2024-12-02 19:10:09 +0900

    JavaScriptによる価格計算

---
+++ app/javascript/price.js
@@ -0,0 +1,13 @@
+function count (){
+  const itemPrice = document.getElementById("item-price");
+  itemPrice.addEventListener("keyup", () => {
+    const countTax = Math.floor(itemPrice.value * 0.1);
+    const countProfit = itemPrice.value - countTax;
+    const addTaxPrice = document.getElementById("add-tax-price");
+    const profit = document.getElementById("profit");
+    addTaxPrice.innerHTML = `${countTax}`;
+    profit.innerHTML = `${countProfit}`;
+  });
+};
+
+window.addEventListener('turbo:load', count);
