0a18d6ede768e27f120ef9ac9e570c44469ebfa9
gessodog <kai.okumura56@gmail.com>
2024-12-11 19:31:16 +0900

    Rubocopの適用

--- app/models/form_buy.rb
+++ app/models/form_buy.rb
@@ -5,7 +5,7 @@ class FormBuy
   with_options presence: true do
     validates :token
     validates :zipcode
-    validates :prefecture_id,    numericality: { other_than: 1, message: "can't be blank" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :house_number
     validates :telephone_number
